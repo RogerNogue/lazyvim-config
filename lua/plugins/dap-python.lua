fa49e1ea84a3e2ac4087055c627ab0acc5fd1c93
rogernogue <rogernogueb@gmail.com>
2025-09-29 10:53:26 +0200

    fix: adapted python path to stop needing virtual environments

--- lua/plugins/dap-python.lua
+++ lua/plugins/dap-python.lua
@@ -4,7 +4,7 @@ return {
     ft = "python",
     dependencies = { "mfussenegger/nvim-dap" },
     config = function()
-      local path = "~/.virtualenvs/debugpy/bin/python" -- ajusta a tu entorno real
+      local path = "/usr/bin/python3"
       require("dap-python").setup(path)
     end,
   },
