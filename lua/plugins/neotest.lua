134e4c78de56ce79720fb52775423b24920e0f1a
rogernogue <rogernogueb@gmail.com>
2025-09-29 10:48:42 +0200

    plugins: added neotest plugin with config

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,57 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/neotest-python",
+  },
+  config = function(_, opts)
+  opts.adapters = {
+    require("neotest-python")({
+        -- Extra arguments for nvim-dap configuration
+        -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+        dap = { justMyCode = false },
+        -- Command line arguments for runner
+        -- Can also be a function to return dynamic values
+        args = {"--log-level", "DEBUG"},
+        -- Runner to use. Will use pytest if available by default.
+        -- Can be a function to return dynamic value.
+        runner = "pytest",
+        -- Custom python path for the runner.
+        -- Can be a string or a list of strings.
+        -- Can also be a function to return dynamic value.
+        -- If not provided, the path will be inferred by checking for 
+        -- virtual envs in the local directory and for Pipenev/Poetry configs
+       -- python = ".venv/bin/python",
+        python = "python"
+        -- Returns if a given file path is a test file.
+        -- NB: This function is called a lot so don't perform any heavy tasks within it.
+    })
+  }
+    require("neotest").setup(opts)
+  end,
+  -- stylua: ignore
+  keys = {
+    {"<leader>t", "", desc = "+test"},
+    { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File (Neotest)" },
+    { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files (Neotest)" },
+    { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest (Neotest)" },
+    { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last (Neotest)" },
+    { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary (Neotest)" },
+    { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output (Neotest)" },
+    { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel (Neotest)" },
+    { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop (Neotest)" },
+    { "<leader>tw", function() require("neotest").watch.toggle(vim.fn.expand("%")) end, desc = "Toggle Watch (Neotest)" },
+  },
+}
